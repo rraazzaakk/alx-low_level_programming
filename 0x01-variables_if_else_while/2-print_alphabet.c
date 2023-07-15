94da6556103d2ca4c041eefd3db367bc497f1bea
rraazzaakk <agboolaoluwatomisin51@gmail.com>
2023-07-14 21:37:07 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+	char c = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
-	if ((n % 10) == 0)
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	else if ((n % 10) > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-
+	putchar('\n');
 	return (0);
 }
